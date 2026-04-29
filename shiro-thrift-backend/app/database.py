b038a61a49ad07c9f74dc8c5a65ed822b11bd0fe
Brian Ratemo Magucha <brianratemom@gmail.com>
2026-04-29 10:46:02 +0300

    Fix database connection for Render

--- shiro-thrift-backend/app/database.py
+++ shiro-thrift-backend/app/database.py
@@ -1,13 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine= create_engine(DATABASE_URL)
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL= DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+
+engine= create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(bind=engine)
 
